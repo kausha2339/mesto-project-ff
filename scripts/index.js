9a3c32a77580298c58b5e22af9679b765e37399e
Kausha Love <kausha2339@yandex.ru>
2024-06-30 16:56:53 +0300

    Version 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,53 @@
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+    const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardDeleteButton = cardItem.querySelector(".card__delete-button");
+    const cardImg = cardItem.querySelector(".card__image");
+    cardImg.src=card.link;
+    cardImg.alt=card.name;
+    cardItem.querySelector(".card__image").src = card.link;
+    cardItem.querySelector(".card__title").textContent = card.name;
+    cardDeleteButton.addEventListener("click", () => {
+        deleteCard(cardItem);
+    });
+    return cardItem;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardItem) {
+    cardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+    const cardElement = createCard(card, deleteCard);
+    cardsContainer.appendChild(cardElement);
+});
+
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const editPopup = document.querySelector('.popup_type_edit');
+const addPopup = document.querySelector('.popup_type_new-card');
+const closeButtons = document.querySelectorAll('.popup__close');
+const overlay = document.querySelectorAll('.popup');
+
+function openPopup(element) {
+    element.classList.add('popup_opened');
+};
+
+function closePopup(element) {
+    element.classList.remove('popup_opened');
+};
+
+editButton.addEventListener('click', () => openPopup(editPopup));
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => closePopup(editPopup));
+});
