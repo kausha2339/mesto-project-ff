c15da3ef1be704d3763814eb70cb6b3650481edd
nwtadmin <kausha2339@yandex.ru>
2024-07-25 17:03:17 +0300

    Version p6 4(modal)

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,18 @@
 export const openPopUp = (modal) => { 
-    modal.classList.add("popup_is-opened");
+    modal.classList.add("popup_is-opened");  
+    modal.classList.add("popup_is-animated");  // сначала анимация
+  setTimeout(() => {
+    modal.classList.add("popup_is-opened");  // потом только открытие
+  }, 1);
     document.addEventListener('keydown', handleEscape);
 }
   
 export function closePopUp (modal) {
+    modal.classList.remove("popup_is-opened");  
+    modal.classList.add("popup_is-animated");  // сначала анимация
+    setTimeout(() => {
     modal.classList.remove("popup_is-opened");
+  }, 1);
     document.removeEventListener('keydown', handleEscape);
 }
 
